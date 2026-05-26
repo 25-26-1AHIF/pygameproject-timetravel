72cd128d0011bb078135a57727649465aa1d5e0f
Efe Yasar <efe.yasar@student.htl-rankweil.at>
2026-05-26 11:10:38 +0200

    Diary_Seite geschrieben

--- src/game/diary.py
+++ src/game/diary.py
@@ -3,20 +3,18 @@ import pygame
 
 def diary(screen: pygame.Surface, filepath: str):
     diary_bild = pygame.image.load(filepath).convert()
-    #screen.blit(diary_bild, dest=(GV.SCREEN_WIDTH/2 - diary_bild_breite/2, GV.SCREEN_HEIGHT/2 - diary_bild_breite/2))
-
     #KI_Anfang;
     #benutzte KI: Microsoft Copilot
     # Prompt: Wie kann ich in Pycharm ein Sprite vergrößert darstellen,
     # ohne dass sich die Pixel des Bildes ändern? Geht das?
     # Muss ich dafür ein neues Sprite erstellen?
-
     scale_factor = 10  # z.B. 4x größer
     width = diary_bild.get_width() * scale_factor
     height = diary_bild.get_height() * scale_factor
-
     sprite_big = pygame.transform.scale(diary_bild, (width, height))
-
     screen.blit(sprite_big, dest=(GV.SCREEN_WIDTH/2 - width/2, GV.SCREEN_HEIGHT/2 - height/2))
-
     # KI-Ende
+    titel_text_tagebuch = GV.FONT_MIDDLE.render("Irgendwann im Mittelalter", True, "white")
+    titel_text_tagebuch_rect = titel_text_tagebuch.get_rect(center=(GV.SCREEN_WIDTH/2 - width/2, GV.SCREEN_HEIGHT/2 - height/2))
+    pygame.draw.rect(surface=screen, rect=titel_text_tagebuch_rect, color="darkgreen",width=0)
+    screen.blit(source=titel_text_tagebuch, dest=titel_text_tagebuch_rect)
