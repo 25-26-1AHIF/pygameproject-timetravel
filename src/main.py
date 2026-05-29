fa30a0f6d90980c1838dc144595ad59e11030de2
Efe Yasar <efe.yasar@student.htl-rankweil.at>
2026-05-29 12:29:14 +0200

    cleaning up

--- src/main.py
+++ src/main.py
@@ -308,42 +308,17 @@ def play_screen(
 
 
 def main():
-
     GV.init()
-
-    screen = pygame.display.set_mode(
-        (GV.SCREEN_WIDTH, GV.SCREEN_HEIGHT)
-    )
-
+    screen = pygame.display.set_mode((GV.SCREEN_WIDTH, GV.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
     GameScreens.actual = GameScreens.MAIN
-
     while GameScreens.actual is not None:
-
         if GameScreens.actual == GameScreens.MAIN:
-
-            GameScreens.actual = main_screen(
-                screen,
-                clock
-            )
-
+            GameScreens.actual = main_screen(screen, clock)
         elif GameScreens.actual == GameScreens.PLAY:
-
-            GameScreens.actual = play_screen(
-                screen,
-                clock,
-                load_save=False
-            )
-
+            GameScreens.actual = play_screen(screen, clock, load_save=False)
         elif GameScreens.actual == GameScreens.LADEN:
-
-            GameScreens.actual = play_screen(
-                screen,
-                clock,
-                load_save=True
-            )
-
+            GameScreens.actual = play_screen(screen, clock, load_save=True)
     pygame.quit()
 
 
