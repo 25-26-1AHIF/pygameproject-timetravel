d884d007b673006fb7fb3e75526fc31439590a84
Efe Yasar <efe.yasar@student.htl-rankweil.at>
2026-05-31 13:43:53 +0200

    Diary auf den Tisch platziert

--- src/game/Attic.py
+++ src/game/Attic.py
@@ -10,6 +10,12 @@ def Attic(screen: pygame.Surface):
     sheet = pygame.image.load("assets/Sprites/Indoor/Tilesheets/rogueLikeIndoor_transparent.png").convert_alpha()
     table_up = sheet.subsurface((102, 153, 16, 17))
     table_down = sheet.subsurface((102, 170, 16, 17))
+    diary_closed = pygame.image.load("assets/Sprites/Diary/Diary_geschlossen.png")
+    diary_closed = pygame.transform.scale(diary_closed, (60,60))
+    mini_font = pygame.font.SysFont("Georgia",10)
+    mini_text = mini_font.render("Diary", True, "#f09941")
+    mini_text_rect = mini_text.get_rect(center=(827, 460))
+
 
     # KI Anfang
     # benutzte KI: Microsoft Copilot
@@ -22,10 +28,12 @@ def Attic(screen: pygame.Surface):
     table.blit(table_down, (0, 16))
     # KI Ende
 
-    table = pygame.transform.scale(table, (85, 140))
+    table = pygame.transform.scale(table, (120, 200))
     table_rect = pygame.Rect(820, 380, 30, 60)
     screen.blit(attic, (0, 0))
     screen.blit(table, (795, 360))
+    screen.blit(diary_closed, (785, 440))
+    screen.blit(mini_text, mini_text_rect)
     screen.blit(stairs, (180, 300))
 
     return table_rect, stairs_rect
